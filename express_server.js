5bbd24800d7cbc8bb9eb3b479d329e8e62f2ca51
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2022-09-07 16:32:35 -0700

    Added login/logout functionality based on username only

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
 
 function generateRandomString() {
   return Math.random().toString(36).substr(2, 6)
@@ -9,6 +10,7 @@ function generateRandomString() {
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -31,16 +33,28 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+    // ... any other vars
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+  };
+  // console.log(templateVars.username);
+
   res.render("urls_show", templateVars);
 });
 
@@ -72,6 +86,11 @@ app.post("/urls/:id/delete", (req, res) => {
 
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
+  res.redirect('/urls/');
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect('/urls/');
 })
 
